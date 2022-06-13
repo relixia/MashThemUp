bae3a0f2bc13e26d6e42499d23f18de107f2041d
Buğra Çayır <bugra-net@hotmail.com>
2022-06-14 00:23:40 +0300

    MashThemUp Unity Project folder

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletManager : MonoBehaviour
+{
+    public float bulletDamage, lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
